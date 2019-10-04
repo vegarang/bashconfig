1b1eb212386216954ea8ea37da28c9eeb45ef50e
Vegard Angell <vegard@appresso.no>
2019-10-04 11:26:37 +0200

    add command to list yarn-linked packages in current project

--- devcommands.sh
+++ devcommands.sh
@@ -351,3 +351,9 @@ resetproject() {
     devinstall ${@:4}
   fi
 }
+
+listyarnlink () {
+  find node_modules node_modules/\@* -depth 1 -type l -print | while read MODULE ; do
+    echo "Linked module in use: $MODULE"
+  done
+}
